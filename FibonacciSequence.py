2ae0679aee835df96c5d7deb2d054c11808055b3
Aditya Mitra <adityamitra1089@gmail.com>
2019-10-19 13:33:29 -0700

    Added FiboList to store previously computed Fibonacci numbers. This has dramtically speeded up computation of high Fibonacci numbers.

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -4,15 +4,29 @@ print('Input a number')
 number = input()
 number = int(number)
 
+FiboList = []
+
 def fibonacci(n):
+    # Is the value in FiboList?
+    length = len(FiboList)
+    if length >= (n+1):
+        # Value is in FiboList
+        retval = FiboList[n]
+        return retval
+
+    # Value not in FiboList. Compute it.
     if n <= 1:
-        return n
+        retval = n
     else:
-        return (fibonacci(n-1)+fibonacci(n-2))
+        retval = (fibonacci(n-2)+fibonacci(n-1))
+
+    # Append to FiboList and return
+    FiboList.append(retval)
+    return retval
 
 if number <= 0:
     print('Please input a positive number')
 else:
     print('Fibonaci Sequence:')
     for i in range(number):
-        print(fibonnacci(i))
+        print(fibonacci(i))
